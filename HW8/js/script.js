59385f1075ba7491e1fbfbaef5657fade6422516
DXSKULL <dxskull9@gmail.com>
2024-12-01 18:09:20 +0500

    hw8

---
+++ HW8/js/script.js
@@ -0,0 +1,37 @@
+// // 1
+// const firstName = "Margulan"
+// const lastName = "Zhumabek"
+// const age = 21
+
+// console.log(`Меня зовут ${firstName} ${lastName}, и мне ${age} лет`)
+
+// // 2 
+// const city = "Almaty"
+// console.log("Я родом из города " + city)
+
+// // 3 
+// let password = "s" // если строка пустая, то false. если нет, то true
+// let hasAccess = !!password
+// console.log(hasAccess)
+// console.log(password)
+
+// // 4 
+// let isMember = false
+// isMember = isMember.toString()
+// console.log(isMember)
+// console.log(typeof isMember)
+
+// // 5 
+// let cartItems = -1 // если 0, то false. остальное все true
+// cartItems = !!cartItems
+// console.log(cartItems)
+
+// // 6 
+// let averageScore = 89.75695
+// averageScore = averageScore.toFixed(1)
+// console.log(averageScore)
+
+// // 7 
+// let greetingMessage = "      Приветствуем вас в мире программирования!        "
+// console.log(greetingMessage.trim().toUpperCase())
+// console.log(greetingMessage.includes("программирования"))
